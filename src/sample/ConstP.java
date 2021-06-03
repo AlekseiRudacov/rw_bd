bc961d510a9a5caf472d9686bccfa4b325e8e291
AlekseiRudacov <75729139+AlekseiRudacov@users.noreply.github.com>
2021-06-03 12:19:56 +0300

    Create ConstP.java

---
+++ src/sample/ConstP.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class ConstP {
+    public static final String RW_TABLE_P="rw_bd";
+
+    public static final String RW_ID_P="id";
+    public static final String RW_START_DATA_PASSENGERS="start_data_passenger";
+    public static final String RW_START_TIME_PASSENGERS="start_time_passenger";
+
+    public static final String RW_WHERE_FROM="where_from";
+    public static final String RW_WHERE_="where_";
+
+    public static final String RW_TRAIN_NUMBER="train_namber";
+
+    public static final String RW_START_TIME_TRAIN="start_time_train";
+    public static final String RW_STOP_TIME_TRAIN="stop_time_train";
+
+    public static final String RW_END_DATA_TRIP1="end_data_trip1";
+    public static final String RW_END_TIME_TRIP1="end_time_trip1";
+}
