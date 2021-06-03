cf85e4ec48fa3d61725711ebc140a1492319ba19
AlekseiRudacov <75729139+AlekseiRudacov@users.noreply.github.com>
2021-06-03 12:20:01 +0300

    Create Const.java

---
+++ src/sample/Const.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class Const {
+    public static final String RW_TABLE="trips";
+
+    public static final String RW_ID="id";
+    public static final String RW_START_DATA_TRIP="start_data_trip";
+    public static final String RW_START_TIME_TRIP="start_time_trip";
+    public static final String RW_START_DATA_WORK="start_data_work";
+    public static final String RW_START_TIME_WORK="start_time_work";
+    public static final String RW_WHERE_FROM="where_from";
+    public static final String RW_WHERE_="where_";
+    public static final String RW_TRAIN_NUMBER="train_namber";
+    public static final String RW_TRAIN_LONG="train_long";
+    public static final String RW_START_SECTION_A="start_section_A";
+    public static final String RW_START_SECTION_B="start_section_B";
+    public static final String RW_END_SECTION_A="end_section_A";
+    public static final String RW_END_SECTION_B="end_section_B";
+    public static final String RW_END_DATA_WORK="end_data_work";
+    public static final String RW_END_TIME_WORK="end_time_work";
+    public static final String RW_END_DATA_TRIP="end_data_trip";
+    public static final String RW_END_TIME_TRIP="end_time_trip";
+
+}
